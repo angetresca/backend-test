1863d5e8e8c6745fb97d327c3ce0a4f69b395497
Ange Tresca <angetresca@gmail.com>
2021-08-14 14:51:55 -0300

    fix: add not found middleware

--- app/index.js
+++ app/index.js
@@ -6,10 +6,16 @@ const routes = require("./routes");
 const app = express();
 
 
+// Body parser middleware
 app.use(express.json());
 
-// Routes
+// Routes middleware
 app.use("/", routes());
+
+// Route not found middleware
+app.use((req, res) => {
+    res.status(404).json({error: "Not found"});
+});
 
 // Host and port
 const host = process.env.HOST || "0.0.0.0";
