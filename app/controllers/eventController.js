2def580dbdcb7665e64b29c5e229c014bdd84398
Ange Tresca <angetresca@gmail.com>
2021-08-14 13:55:05 -0300

    feat: run linter

--- app/controllers/eventController.js
+++ app/controllers/eventController.js
@@ -1,18 +1,18 @@
 const events = require("../events.json");
 
 exports.eventsHome = async (req, res) => {
-    res.send("<h1>HOLA MUNDO</h1>")
-}
+    res.send("<h1>HOLA MUNDO</h1>");
+};
 
 exports.getEvents = async (req, res) => {
     res.json(events);
-}
+};
 
 exports.createNewEvent = async (req, res) => {
     const data = req.body;
 
     if (!data || !data.name) {
-        return res.status(400).json({error: "event.name is missing"})
+        return res.status(400).json({error: "event.name is missing"});
     }
 
     const ids = events.map(ev => ev.id);
@@ -24,20 +24,20 @@ exports.createNewEvent = async (req, res) => {
         description: data.description,
         photoUrl: data.photoUrl,
         dates: data.dates
-    }
+    };
     //events = [...events, newEvent]  TODO: SAVE NEW EVENT
     res.status(201).json(newEvent);
-}
+};
 
 exports.getEventById = async (req, res) => {
     const id = Number(req.params.id);
     const event = events.find(ev => ev.id === id);
-    if (event) { res.json(event) } else { res.status(404).end("event not found") }
-}
+    if (event) { res.json(event); } else { res.status(404).end("event not found"); }
+};
 
 exports.deleteEventById = async (req, res) => {
     const id = Number(req.params.id);
     const evs = events.find(ev => ev.id !== id);
     //res.status(200).end(`event with id: ${id} was deleted`);
     res.status(200).json(evs);
-}
+};
--- app/routes.js
+++ app/routes.js
@@ -8,27 +8,27 @@ module.exports = function () {
     // home route
     router.get("/",
         eventController.eventsHome
-    )
+    );
 
     // get all events 
     router.get("/api/events",
         eventController.getEvents
-    )
+    );
 
     // create new event
     router.post("/api/events",
         eventController.createNewEvent
-    )
+    );
 
     // event by id
     router.get("/api/events/:id",
         eventController.getEventById
-    )
+    );
 
     // delete event by id
     router.delete("/api/events/:id",
         eventController.deleteEventById
-    )
+    );
 
     return router;
-}
+};
